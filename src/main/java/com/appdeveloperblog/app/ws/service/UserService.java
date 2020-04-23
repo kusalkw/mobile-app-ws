05b4b11991e147773c3b39295d7c4a37b243f971
Kusal Kalhara <kusalkalhara1@gmail.com>
2020-04-24 00:19:10 +0530

    initial commit

---
+++ src/main/java/com/appdeveloperblog/app/ws/service/UserService.java
@@ -0,0 +1,10 @@
+package com.appdeveloperblog.app.ws.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.appdeveloperblog.app.ws.shared.dto.UserDto;
+
+public interface UserService extends UserDetailsService{
+	UserDto createUser(UserDto user);
+	
+}
